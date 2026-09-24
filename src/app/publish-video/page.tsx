96608e8128bae7eed1d1e8babdb0b5ba01c7ac80
lujianing029-design <lujianing029@gmail.com>
2026-09-25 00:57:43 +0800

    feat: keep video upload in publishing workspace

--- src/app/publish-video/page.tsx
+++ src/app/publish-video/page.tsx
@@ -1,5 +1,11 @@
 import { PublishComposer } from "@/components/publishing/publish-composer";
+import { VideoUpload } from "@/components/publishing/video-upload";
 
 export default function PublishVideoPage() {
-  return <PublishComposer />;
+  return (
+    <>
+      <VideoUpload />
+      <PublishComposer />
+    </>
+  );
 }
